24b3e8ad8912ac316f15aa5ff85db6ea37ef5ea1
vladimircherevko <vladymyrcher@gmail.com>
2020-02-06 17:16:06 +0200

    add build

---
+++ client/build/precache-manifest.e2de6a0018088eed1a929b8e0ddddf75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34bf24c678454cc0ddc978b91df505c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b420e436e792062c4a9b",
+    "url": "/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "d0a0fc3180e9cf538277",
+    "url": "/static/css/main.25d125a5.chunk.css"
+  },
+  {
+    "revision": "b420e436e792062c4a9b",
+    "url": "/static/js/2.39dfa9c6.chunk.js"
+  },
+  {
+    "revision": "0897a2af4d47251d787cefe2723205c2",
+    "url": "/static/js/2.39dfa9c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a0fc3180e9cf538277",
+    "url": "/static/js/main.45f3008d.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
